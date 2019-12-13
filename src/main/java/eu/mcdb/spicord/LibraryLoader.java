59c8c7ebd890bdfaf13a17ed50ea5e6549e83a2e
OopsieWoopsie <xiaosheidy@gmail.com>
2019-12-13 02:43:17 -0300

    added license header

--- src/main/java/eu/mcdb/spicord/LibraryLoader.java
+++ src/main/java/eu/mcdb/spicord/LibraryLoader.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2019  OopsieWoopsie
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package eu.mcdb.spicord;
 
 import static eu.mcdb.util.ReflectionUtils.classExists;
@@ -100,7 +117,7 @@ public class LibraryLoader {
         for (Library lib : libraries) {
             if (lib.getDontloadifclassfound() != null) {
                 if (classExists(lib.getDontloadifclassfound())) {
-                    log.info("[Loader] Another plugin already loaded '" + lib.getName() + "', skipping...");
+                    log.info("[Loader] The library '" + lib.getName() + "' wasn't loaded by Spicord, errors may occur.");
                     continue;
                 }
             }
@@ -124,7 +141,7 @@ public class LibraryLoader {
     }
 
     @Getter
-    private class Library implements Serializable {
+    public class Library implements Serializable {
 
         private static final long serialVersionUID = 1L;
 
