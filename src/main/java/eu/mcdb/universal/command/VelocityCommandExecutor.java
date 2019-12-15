38f81c3aab207fb8c900e4aef6ebd79d81b911a0
OopsieWoopsie <xiaosheidy@gmail.com>
2019-12-15 17:15:02 -0300

    added license header

--- src/main/java/eu/mcdb/universal/command/VelocityCommandExecutor.java
+++ src/main/java/eu/mcdb/universal/command/VelocityCommandExecutor.java
@@ -1,9 +1,32 @@
+/*
+ * Copyright (C) 2019  OopsieWoopsie
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package eu.mcdb.universal.command;
 
 import com.velocitypowered.api.command.Command;
 import com.velocitypowered.api.command.CommandSource;
 import net.kyori.text.TextComponent;
 
+/**
+ * Wrapper for the {@link UniversalCommand} class to
+ * make it usable by Velocity.
+ * 
+ * @author sheidy
+ */
 public final class VelocityCommandExecutor implements Command {
 
     private final UniversalCommand command;
