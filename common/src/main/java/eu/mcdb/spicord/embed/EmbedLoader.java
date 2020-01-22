e810486825676e7977955febd57be41a8a1957f6
OopsieWoopsie <xiaosheidy@gmail.com>
2020-01-22 19:49:57 -0300

    added utility for file extraction

--- common/src/main/java/eu/mcdb/spicord/embed/EmbedLoader.java
+++ common/src/main/java/eu/mcdb/spicord/embed/EmbedLoader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019  OopsieWoopsie
+ * Copyright (C) 2020  OopsieWoopsie
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -24,9 +24,10 @@ import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 import com.google.common.base.Preconditions;
-import eu.mcdb.spicord.Spicord;
+import eu.mcdb.spicord.api.Node;
+import eu.mcdb.util.ZipExtractor;
 
-public class EmbedLoader {
+public class EmbedLoader implements Node {
 
     private static final Charset charset = Charset.forName("UTF-8");
     private final Map<String, Embed> embeds;
@@ -35,26 +36,68 @@ public class EmbedLoader {
         this.embeds = new HashMap<String, Embed>();
     }
 
-    public void load(File dir) {
+    /**
+     * Loads the embed files located into the {@code dir}.
+     * 
+     * @param dir the directory
+     */
+    public void load(final File dir) {
         Preconditions.checkArgument(dir.isDirectory(), "dir");
 
-        for (File f : dir.listFiles()) {
-            String name = f.getName().trim();
+        for (final File file : dir.listFiles()) {
+            String name = file.getName().trim();
 
-            if (f.isFile() && name.endsWith(".json")) {
+            if (file.isFile() && name.endsWith(".json")) {
                 try {
                     name = name.substring(0, name.length() - 5).trim();
-                    String content = new String(Files.readAllBytes(f.toPath()), charset);
+                    final String content = new String(Files.readAllBytes(file.toPath()), charset);
                     embeds.put(name, Embed.fromJson(content));
                 } catch (IOException e) {
-                    Spicord.getInstance().getLogger().warning("Cannot load the embed '" + f.getName() + "'.");
+                    getLogger().warning("Cannot load the embed '" + file.getName() + "'.");
                     e.printStackTrace();
                 }
             }
         }
     }
 
-    public Embed getEmbedByName(String name) {
+    /**
+     * This method extracts the json (embed) files located inside the "embed" folder
+     * which should be at the root of the jar/zip {@code file}. <br>
+     * A file will be extracted only if it don't exist into the {@code out} folder.
+     * <br>
+     * After that, all the embed files located into the {@code out} folder will be
+     * loaded into a new {@link EmbedLoader} instance.
+     * 
+     * @param file the file jar file
+     * @param out  the output directory
+     * @return the {@link EmbedLoader} instance with the loaded embeds
+     * @throws IOException if an I/O error has occurred
+     */
+    public static EmbedLoader extractAndLoad(final File file, final File out) throws IOException {
+        out.mkdirs();
+        Preconditions.checkArgument(out.isDirectory(), "out");
+
+        final ZipExtractor ex = new ZipExtractor(file);
+        ex.filter("embed\\/.*\\.json");
+        ex.extract(out);
+        ex.close();
+
+        final EmbedLoader loader = new EmbedLoader();
+        loader.load(out);
+
+        return loader;
+    }
+
+    /**
+     * Gets an embed instance previously loaded by using its name.
+     * <br>
+     * The embed name has the same name as its file but without the .json extension.
+     * 
+     * @see {@link #load(File)}
+     * @param name the embed name
+     * @return the {@link Embed} instance
+     */
+    public Embed getEmbedByName(final String name) {
         return embeds.get(name);
     }
 }
---
+++ common/src/main/java/eu/mcdb/util/ZipExtractor.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2020  OopsieWoopsie
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package eu.mcdb.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+
+public class ZipExtractor implements AutoCloseable {
+
+    private final JarFile zipFile;
+    private final List<ZipEntry> entries;
+
+    /**
+     * 
+     * 
+     * @param file the zip file
+     * @throws IOException if an I/O error has occurred
+     */
+    public ZipExtractor(final File file) throws IOException {
+        this.zipFile = new JarFile(file);
+        this.entries = new ArrayList<ZipEntry>();
+
+        final Enumeration<JarEntry> e = zipFile.entries();
+
+        while (e.hasMoreElements()) {
+            entries.add(e.nextElement());
+        }
+    }
+
+    /**
+     * Filters all the zip entries by using a pattern.
+     * <br>
+     * All the entries that matches with the pattern will be available for
+     * extraction and the ones who didn't matched will be ignored.
+     * 
+     * @see {@link #extract(File)}
+     * @param regex the regex pattern
+     */
+    public void filter(final String regex) {
+        final Pattern pattern = Pattern.compile(regex);
+        entries.removeIf(entry -> !pattern.matcher(entry.getName()).find());
+    }
+
+    /**
+     * Extracts all the zip files into the {@code folder}.
+     * <br>
+     * If a filter was applied, only the filtered files will be extracted.
+     * 
+     * @see {@link #filter(String)}
+     * @param out the output folder to extract the files
+     * @throws IOException if an I/O error has occurred
+     */
+    public void extract(final File out) throws IOException {
+        for (final ZipEntry entry : entries) {
+            String name = entry.getName();
+            name = name.substring(name.lastIndexOf('/') + 1);
+
+            final File file = new File(out, name);
+
+            if (!file.exists()) {
+                file.createNewFile();
+                Files.copy(zipFile.getInputStream(entry), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            }
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        entries.clear();
+        zipFile.close();
+    }
+}
