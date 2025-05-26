4813aaa6f7f41cb0b92ed82caaba98dc9bbab9a1
anvorgesa <95182588+anvorgesa@users.noreply.github.com>
2025-05-26 00:31:11 -0300

    Version 5.7.0

--- minecraft/src/main/java/org/spicord/velocity/SpicordVelocity.java
+++ minecraft/src/main/java/org/spicord/velocity/SpicordVelocity.java
@@ -36,7 +36,7 @@ import com.velocitypowered.api.proxy.ProxyServer;
 import eu.mcdb.universal.Server;
 import eu.mcdb.universal.ServerType;
 
-@Plugin(id = "spicord", name = "Spicord", version = "5.6.1", authors = { "Tini" })
+@Plugin(id = "spicord", name = "Spicord", version = "5.7.0", authors = { "Tini" })
 public class SpicordVelocity extends VelocityPlugin implements SpicordPlugin {
 
     private SpicordLoader loader;
